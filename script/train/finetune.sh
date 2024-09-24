84b81c6670bd6e984980b0784fe93373e0c4754b
pw-02 <watters.patrick@outlook.com>
2024-09-24 08:51:34 -0700

    pargs changed

--- script/train/finetune.sh
+++ script/train/finetune.sh
@@ -7,14 +7,14 @@ mkdir -p ${OUTPUT_DIR_FT}
 
 deepspeed --include localhost:0,1,2,3,4,5,6,7 vita/train/train.py \
     --deepspeed ./script/deepspeed/ds_config_zero3_offload2.json \
-    --model_name_or_path Mixtral-8x7B_modVocab/mg2hg \
+    --model_name_or_path downloads/VITA_ckpt \
     --model_type $MODEL_TYPE \
     --version mixtral_two \
     --dataset_use Pretrain_video \
-    --vision_tower InternViT-300M-448px \
+    --vision_tower downloads/InternViT-300M-448px \
     --pretrain_mm_mlp_adapter ${OUTPUT_DIR}/llava-s1-pretrain_mlp_video/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
-    --audio_encoder audio-encoder-2wh_zh_en_audioset_Mixtral-8x7B_New-base-tunning \
+    --audio_encoder downloads/audio-encoder-2wh_zh_en_audioset_Mixtral-8x7B_New-base-tunning \
     --freeze_audio_encoder True \
     --freeze_audio_encoder_adapter False \
     --image_aspect_ratio square \
