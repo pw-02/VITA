7a01c1152507b95f93480c30fc32dc139c39244c
pw-02 <watters.patrick@outlook.com>
2024-09-24 11:04:27 -0700

    path change

--- downlaod_model.py
+++ downlaod_model.py
@@ -1,6 +1,11 @@
 from transformers import AutoTokenizer, AutoModel
 
-# Load the tokenizer and model
-model_name = "downloads/VITA_ckpt"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+model_name = "downloads/InternViT-300M-448px"
+model_name = "downloads/audio-encoder-2wh_zh_en_audioset_Mixtral-8x7B_New-base-tunning"
+
+# Load the tokenizer
+# tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+# Load the model
 model = AutoModel.from_pretrained(model_name)
+pass
