b3a99acf7b49ee5db7f2ef44c9e9f7bd8f995cef
pw-02 <watters.patrick@outlook.com>
2024-09-24 11:58:51 -0700

    downlaods added

--- downlaod_model.py
+++ downlaod_model.py
@@ -1,10 +1,9 @@
 from transformers import AutoTokenizer, AutoModel
 
-model_name = "downloads/InternViT-300M-448px"
-model_name = "downloads/audio-encoder-2wh_zh_en_audioset_Mixtral-8x7B_New-base-tunning"
+model_name = "downloads\VITA_ckpt"
 
 # Load the tokenizer
-# tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
 
 # Load the model
 model = AutoModel.from_pretrained(model_name)
